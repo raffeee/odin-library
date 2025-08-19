d4c49e31f2af2aca396dbf8c8cd82fb855407c7e
Rafael Reyes <100984393+raffeee@users.noreply.github.com>
2025-08-19 14:10:23 +0800

    feat: implement Book constructor and addBookToLibrary()

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book(title, author, pagecount, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call this constructor.")
+    }
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pagecount = pagecount;
+    this.read = read;
+    this.info = function() {
+        return `${title} by ${author}, ${pagecount} pages, ${read}`
+    }
+}
+
+function addBookToLibrary(title, author, pagecount, read) {
+    myLibrary.push(new Book(title, author, pagecount, read))
+}
